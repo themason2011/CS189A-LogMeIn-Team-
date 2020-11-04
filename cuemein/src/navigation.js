a9664f790dad40418d77b531e0c477fc5392f83f
jjimenez98 <59632440+jjimenez98@users.noreply.github.com>
2020-11-03 23:44:50 -0800

    added react-app

---
+++ cuemein/src/navigation.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Nav() {
+    return(
+    <div>
+        <nav>
+            <label>CueMEin</label>
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+        </nav>
+    </div>
+    )
+}
+
+
+export default Nav
