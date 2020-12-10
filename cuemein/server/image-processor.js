3b4e57dbc7cd9bb5ae87ff9780c3edf0d8e0436b
Juan Mendoza-Martinez <juanmendozamartinez@cs.ucsb.edu>
2020-12-10 10:41:38 -0800

    updated image process function for blob data

--- cuemein/server/image-processor.js
+++ cuemein/server/image-processor.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const axios = require('axios').default;
 
-async function ProcessImage(image) {
+async function ProcessImage(blob_data) {
     // Add a valid subscription key and endpoint to your environment variables.
     //NOTE: YOU MUST RUN THE FOLLOWING:
     // set COGNITIVE_SERVICE_KEY=053718cf44924eb1a7b9078cd0ba14ec
@@ -15,12 +15,13 @@ async function ProcessImage(image) {
     // Optionally, replace with your own image URL (for example a .jpg or .png URL).
     let imageUrl = "https://docs.microsoft.com/en-us/learn/data-ai-cert/identify-faces-with-computer-vision/media/clo19_ubisoft_azure_068.png"
     
+    
     // Send a POST request
     return await axios({
         method: 'post',
         url: endpoint,
         headers : {
-            'Content-Type' : 'application/json',
+            'Content-Type' : 'application/octet-stream',
             'Ocp-Apim-Subscription-Key': subscriptionKey
         },
         params : {
@@ -28,9 +29,7 @@ async function ProcessImage(image) {
             returnFaceAttributes: 'emotion',
             returnFaceId: true
         },
-        data: {
-            url: imageUrl
-        }
+        data: blob_data
         }).then(function (response) {
 
             let prediction, max_val = 0;
