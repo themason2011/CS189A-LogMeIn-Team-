7cbf92df17227aaa039d125c9c17ef205fc6c092
Javier Jimenez <javierjimenez@Javiers-MacBook-Pro.local>
2021-09-22 13:40:47 -0700

    build

---
+++ cuemein/build/precache-manifest.aac048451373501363b9c51698bcdeaa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4db44f2841d339745d52fd6ea254173",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c96288199853d88efa5e",
+    "url": "/static/css/2.c9e7500d.chunk.css"
+  },
+  {
+    "revision": "c2b5284c06c8a15a53b3",
+    "url": "/static/css/main.94b30ada.chunk.css"
+  },
+  {
+    "revision": "c96288199853d88efa5e",
+    "url": "/static/js/2.3cf8b96f.chunk.js"
+  },
+  {
+    "revision": "a7aec927f1e863a53de22e58e5329447",
+    "url": "/static/js/2.3cf8b96f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b5284c06c8a15a53b3",
+    "url": "/static/js/main.1a58bd73.chunk.js"
+  },
+  {
+    "revision": "5a365a053e30ce8bcb2f",
+    "url": "/static/js/runtime-main.fe020acb.js"
+  },
+  {
+    "revision": "74afff2e8f56b09a6da3646f31953bee",
+    "url": "/static/media/11.74afff2e.jpg"
+  }
+]);
